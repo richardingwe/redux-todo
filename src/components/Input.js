70fc96349642b6d398b543b4b4c96fb1c30aab17
richardingwe <richardingwe5@gmail.com>
2021-06-29 12:41:20 +0100

    finished with the todoReducer

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import './Input.css';
 
+import { useDispatch } from 'react-redux';
+import { saveTodo } from '../features/todoSlice';
+
 const Input = () => {
 
     const [input, setInput] = useState('');
+    const dispatch = useDispatch();
 
     const addTodo = () => {
-
+        dispatch(saveTodo({
+            item: input,
+            done: false,
+            id: Date.now()
+        }));
     };
 
     return (
